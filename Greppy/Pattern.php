5dcda64956c8e9078e3c926838041041ee7c8f9f
Henrique Moody <henriquemoody@gmail.com>
2013-03-05 21:16:07 -0300

    Fixed some coding standards

--- Greppy/Pattern.php
+++ Greppy/Pattern.php
@@ -19,78 +19,91 @@ class Pattern
     public function any()
     {
         $this->symbols .= '.';
+
         return $this;
     }
 
     public function literal($literal)
     {
         $this->symbols .= $literal;
+
         return $this;
     }
 
     public function range($start, $end)
     {
         $this->symbols .= "$start-$end";
+
         return $this;
     }
 
     public function alternatives(array $alternatives)
     {
         $this->symbols .= implode('|', $alternatives);
+
         return $this;
     }
 
     public function word()
     {
         $this->symbols .= '\w';
+
         return $this;
     }
 
     public function nonWord()
     {
         $this->symbols .= '\W';
+
         return $this;
     }
 
     public function whitespace()
     {
         $this->symbols .= '\s';
+
         return $this;
     }
 
     public function nonWhitespace()
     {
         $this->symbols .= '\S';
+
         return $this;
     }
 
     public function digit()
     {
         $this->symbols .= '\d';
+
         return $this;
     }
 
     public function nonDigit()
     {
         $this->symbols .= '\D';
+
         return $this;
     }
 
     public function zeroOrMore()
     {
         $this->symbols .= '*';
+
         return $this;
     }
 
     public function oneOrMore()
     {
         $this->symbols .= '+';
+
         return $this;
     }
 
     public function optional()
     {
         $this->symbols .= '?';
+
         return $this;
     }
 
@@ -98,4 +111,5 @@ class Pattern
     {
         return $this->delimiter . $this->symbols . $this->delimiter;
     }
+
 }
