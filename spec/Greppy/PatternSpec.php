e7a5e6664187925b31368711425feae9599f1352
Daniel Ribeiro <drgomesp@gmail.com>
2014-04-04 22:50:29 -0300

    First spec and example passing

---
+++ spec/Greppy/PatternSpec.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the Greppy package.
+ *
+ * (c) Daniel Ribeiro <drgomesp@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace spec\Greppy;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+/**
+ * Pattern spec.
+ *
+ * @author Daniel Ribeiro <drgomesp@gmail.com>
+ * @package spec\Greppy
+ */
+class PatternSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Greppy\Pattern');
+    }
+
+    /**
+     * preg_match("/./", "123. abc ?=+")
+     */
+    function it_should_be_possible_to_match_any_single_character()
+    {
+        $this->any()->match("123. abc ?=+")->shouldReturn(true);
+    }
+}
---
+++ src/Greppy/Pattern.php
@@ -0,0 +1,61 @@
+<?php
+
+/*
+ * This file is part of the Greppy package.
+ *
+ * (c) Daniel Ribeiro <drgomesp@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Greppy;
+
+/**
+ * Class Pattern
+ *
+ * @author Daniel Ribeiro <drgomesp@gmail.com>
+ * @package Greppy
+ */
+class Pattern
+{
+    /**
+     * @var string
+     */
+    const DELIMITER_SLASH = "/";
+    
+    /**
+     * @var string
+     */
+    protected $subject;
+
+    /**
+     * @var string
+     */
+    protected $pattern;
+    
+    /**
+     * Ends the flow by matching an input string subject.
+     * 
+     * @param string $subject
+     * @return bool 
+     */
+    public function match($subject)
+    {
+        $this->subject = $subject;
+        
+        return (bool) preg_match(
+            sprintf("%s%s%s", self::DELIMITER_SLASH, $this->pattern, self::DELIMITER_SLASH), 
+            $subject
+        );
+    }
+
+    /**
+     * @return \Greppy\Pattern
+     */
+    public function any()
+    {
+        $this->pattern .= ".";
+        return $this;
+    }
+}
